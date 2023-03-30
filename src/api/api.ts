ad51f5b80b14617960ad231b9ff5e4107eafc00b
= <=>
2023-03-30 11:07:15 -0300

    return array list

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,23 @@ import createInstance from "./axios.instace";
 export async function listProducts(){
     try {
     const {data} = await createInstance.get('/list/product');
-    return data
+    const list = [{
+        id:'1',
+        name:'Requeijão',
+        category:'Laticinios',
+        description:'Requeijão cremoso maranguape',
+        price:'5,69',
+        uploads:{
+            id:'1',
+            location:'aws url',
+            etag:'etag',
+            bucket:'test',
+            key:'key nome do produto',
+            prodid:'1'
+
+        }
+    }]
+    return list
     } catch (error) {
      return error   
     }
