947abc70b296242121d7ea2062e29364245d1603
= <=>
2023-04-01 15:09:39 -0300

    ajuste footer

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
 const Footer = styled.footer`
-  padding: 20px 0;
   border-top: 0px solid #ccc;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background-color: #309209;
+  color: #fff;
+  position: fixed;
+  width: 100%;
+  height:60px;
+  padding: 20px;
+  bottom: 0;
 `;
 
 export default Footer
